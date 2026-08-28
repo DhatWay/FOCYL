26b853effa040c0481e8029b2016cfbe6ec3db93
iamthevgod <dhatway2024@gmail.com>
2026-08-28 19:19:14 -0400

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,23 @@
+// sw.js – basic offline cache for Focyl
+const CACHE = 'focyl-v1';
+const ASSETS = [
+  '/',
+  '/index.html',
+  '/auth.html',
+  '/board.html',
+  '/manifest.json',
+  '/icon-192.png',
+  '/icon-512.png'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(ASSETS))
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request))
+  );
+});
