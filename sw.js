f0a7b2227d6398b6243d3efdd65df712f283025d
iamthevgod <dhatway2024@gmail.com>
2026-09-04 00:20:08 -0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -8,7 +8,7 @@
 
    Bump CACHE on every deploy.
    ============================================================ */
-const CACHE = 'focyl-v14';
+const CACHE = 'focyl-v15';
 
 const SHELL = [
   './', './index.html', './auth.html', './board.html', './manifest.json',
